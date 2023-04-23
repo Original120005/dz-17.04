c2bc0cd418b534f8e61f69c2dc3b50c4869bd6cc
Dima <112938291+Original120005@users.noreply.github.com>
2023-04-23 21:00:23 +0300

    Update Form1.Designer.cs

--- dz 17.04/Form1.Designer.cs
+++ dz 17.04/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace Миллионер_homework_14._04._2023
+namespace dz17_04
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,280 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "1 - 1000"}, -1, System.Drawing.Color.Empty, System.Drawing.SystemColors.ControlText, new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))));
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "2 - 2000"}, -1, System.Drawing.Color.Empty, System.Drawing.SystemColors.ControlText, new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))));
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "3 - 3000"}, -1, System.Drawing.Color.Empty, System.Drawing.SystemColors.ControlText, new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.администраторскийРежимToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renew = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.D = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hallhelp = new System.Windows.Forms.Button();
+            this.phonehelp = new System.Windows.Forms.Button();
+            this.halfhelp = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.администраторскийРежимToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(978, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(68, 29);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // администраторскийРежимToolStripMenuItem
+            // 
+            this.администраторскийРежимToolStripMenuItem.Name = "администраторскийРежимToolStripMenuItem";
+            this.администраторскийРежимToolStripMenuItem.Size = new System.Drawing.Size(255, 29);
+            this.администраторскийРежимToolStripMenuItem.Text = "Администраторский режим";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(141, 29);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // renew
+            // 
+            this.renew.BackColor = System.Drawing.Color.Black;
+            this.renew.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources._new;
+            this.renew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.renew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.renew.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.renew.Location = new System.Drawing.Point(4, 38);
+            this.renew.Name = "renew";
+            this.renew.Size = new System.Drawing.Size(63, 46);
+            this.renew.TabIndex = 1;
+            this.renew.UseVisualStyleBackColor = false;
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Black;
+            this.exit.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources.Exit;
+            this.exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Location = new System.Drawing.Point(73, 38);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(63, 46);
+            this.exit.TabIndex = 2;
+            this.exit.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(29, 380);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(652, 70);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Question";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // A
+            // 
+            this.A.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.A.ForeColor = System.Drawing.Color.Gold;
+            this.A.Location = new System.Drawing.Point(29, 474);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(311, 62);
+            this.A.TabIndex = 4;
+            this.A.Text = "A:";
+            this.A.UseVisualStyleBackColor = false;
+            // 
+            // B
+            // 
+            this.B.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.B.ForeColor = System.Drawing.Color.Gold;
+            this.B.Location = new System.Drawing.Point(370, 474);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(311, 62);
+            this.B.TabIndex = 5;
+            this.B.Text = "B:";
+            this.B.UseVisualStyleBackColor = false;
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.C.ForeColor = System.Drawing.Color.Gold;
+            this.C.Location = new System.Drawing.Point(29, 562);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(311, 62);
+            this.C.TabIndex = 7;
+            this.C.Text = "C:";
+            this.C.UseVisualStyleBackColor = false;
+            // 
+            // D
+            // 
+            this.D.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.D.ForeColor = System.Drawing.Color.Gold;
+            this.D.Location = new System.Drawing.Point(370, 562);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(311, 62);
+            this.D.TabIndex = 6;
+            this.D.Text = "D:";
+            this.D.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.hallhelp);
+            this.groupBox1.Controls.Add(this.phonehelp);
+            this.groupBox1.Controls.Add(this.halfhelp);
+            this.groupBox1.ForeColor = System.Drawing.Color.Gold;
+            this.groupBox1.Location = new System.Drawing.Point(740, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 74);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подсказки";
+            // 
+            // hallhelp
+            // 
+            this.hallhelp.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hallhelp.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources._3;
+            this.hallhelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hallhelp.Cursor = System.Windows.Forms.Cursors.Default;
+            this.hallhelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hallhelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hallhelp.Location = new System.Drawing.Point(164, 25);
+            this.hallhelp.Name = "hallhelp";
+            this.hallhelp.Size = new System.Drawing.Size(58, 37);
+            this.hallhelp.TabIndex = 2;
+            this.hallhelp.UseVisualStyleBackColor = false;
+            // 
+            // phonehelp
+            // 
+            this.phonehelp.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.phonehelp.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources._2;
+            this.phonehelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.phonehelp.Cursor = System.Windows.Forms.Cursors.Default;
+            this.phonehelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.phonehelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phonehelp.Location = new System.Drawing.Point(88, 25);
+            this.phonehelp.Name = "phonehelp";
+            this.phonehelp.Size = new System.Drawing.Size(58, 37);
+            this.phonehelp.TabIndex = 1;
+            this.phonehelp.UseVisualStyleBackColor = false;
+            // 
+            // halfhelp
+            // 
+            this.halfhelp.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.halfhelp.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources._1;
+            this.halfhelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.halfhelp.Cursor = System.Windows.Forms.Cursors.Default;
+            this.halfhelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.halfhelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.halfhelp.Location = new System.Drawing.Point(11, 25);
+            this.halfhelp.Name = "halfhelp";
+            this.halfhelp.Size = new System.Drawing.Size(58, 37);
+            this.halfhelp.TabIndex = 0;
+            this.halfhelp.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.ForeColor = System.Drawing.Color.Gold;
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "1";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.listView1.Location = new System.Drawing.Point(723, 117);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(248, 553);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Миллионер_homework_14._04._2023.Properties.Resources.mil;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(978, 682);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.renew);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Кто хочет стать миллионером";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem администраторскийРежимToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Button renew;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button halfhelp;
+        private System.Windows.Forms.Button phonehelp;
+        private System.Windows.Forms.Button hallhelp;
+        private System.Windows.Forms.ListView listView1;
     }
 }
